5488672e0058c5c8774a3e162906fb57dea6d0fa
马静宇 <18725001243@163.com>
2021-01-27 16:35:41 +0800

    单例，代理

---
+++ src/main/java/com/example/learnproject/designpattern/factorymethod/AbstractFatoty.java
@@ -0,0 +1,14 @@
+package com.example.learnproject.designpattern.factorymethod;
+
+import com.example.learnproject.designpattern.simplefactory.Phone;
+
+/**
+ * @author ：majingyu
+ * @date ：Created in 2021/1/27 13:20
+ * @description：
+ * @modified By：
+ */
+public interface AbstractFatoty {
+
+    Phone makePhonr();
+}
