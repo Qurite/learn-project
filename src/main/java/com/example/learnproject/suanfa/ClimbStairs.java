11efbe8865adfc21566c0a60692e28505fb43b11
Qurite <mjyu01@163.com>
2021-09-25 22:31:27 +0800

    todo

--- src/main/java/com/example/learnproject/suanfa/ClimbStairs.java
+++ src/main/java/com/example/learnproject/suanfa/ClimbStairs.java
@@ -9,23 +9,44 @@ package com.example.learnproject.suanfa;
  */
 public class ClimbStairs {
     public static int climbStairs(int n) {
-        if (n <= 2) {
+        if (n <= 1) {
             return n;
         }
-
         int a = 1, b = 2;
-        int c = 0;
         for (int i = 3; i <=n; i++) {
-            c = a + b;
+            int c = a + b;
             a = b;
             b = c;
-
         }
         return b;
     }
 
+    /**
+     * 记忆化递归
+     * @param n
+     * @return
+     */
+    public static int climbStairs1(int n) {
+        int[] memo = new int[n + 1];
+        return f(n, memo);
+    }
+
+    private static int f(int n, int[] memo) {
+        if (memo[n] > 0) {
+            return memo[n];
+        }
+        if (n <= 2) {
+            memo[n] = n;
+            return memo[n];
+        } else {
+            memo[n] = f(n - 1, memo) + f(n - 2, memo);
+        }
+        return memo[n];
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(climbStairs(3));
+        System.out.println(climbStairs(8));
+        System.out.println(climbStairs1(8));
     }
 }
--- src/main/java/com/example/learnproject/suanfa/UniquePathSolution.java
+++ src/main/java/com/example/learnproject/suanfa/UniquePathSolution.java
@@ -13,19 +13,42 @@ class UniquePathSolution {
      * @return
      */
     public static int uniquePaths(int m, int n) {
-        int[][] dept = new int[m][n];
+        int[][] dep = new int[m][n];
         for (int i = 0; i < m; i++) {
-            dept[i][0] = 1;
+            dep[i][0] = 1;
         }
         for (int i = 0; i < n; i++) {
-            dept[0][i] = 1;
+            dep[0][i] = 1;
         }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dept[i][j] = dept[i][j - 1] + dept[i - 1][j];
+                dep[i][j] = dep[i - 1][j] + dep[i][j - 1];
             }
         }
-        return dept[m - 1][n - 1];
+        return dep[m - 1][n - 1];
+    }
+
+    /**
+     * 记忆化递归
+     * @param m
+     * @param n
+     * @return
+     */
+    public static int uniquePathsMemo(int m, int n) {
+        // TODO: 2021/9/25  
+        int[][] dep = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dep[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dep[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dep[i][j] = dep[i - 1][j] + dep[i][j - 1];
+            }
+        }
+        return dep[m - 1][n - 1];
     }
 
     public static void main(String[] args) {
